ca932910ac6baa02b0d7cf847e66e85205ece421
gyhou <47550192+gyhou@users.noreply.github.com>
2019-08-29 12:27:09 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,32 +4,26 @@ import numpy as np
 from datetime import *
 from flask import Flask, request, jsonify
 
+
 # def ValuePredictor(to_predict_list):
 #     """Predict function"""
 #     to_predict = np.array(to_predict_list).reshape(1, -1)
-#     loaded_model = pickle.load(open("diabetes_model.pkl", 'rb'))
+#     loaded_model = pickle.load(open("model.pkl", 'rb'))
 #     result = loaded_model.predict(to_predict)
-#     return result[0]
+#     return result
 
 
 app = Flask(__name__)
 @app.route("/", methods=['POST'])
 def address():
-    '''Takes a list of json dictionaries and return average value within
+    '''Takes a list of json dictionaries and return prediction values within
     each time frame'''
 
     # receive input
     inputs = request.get_json(force=True)
-
-    morning_val = 0
-    lunch_val = 0
-    dinner_val = 0
-    bed_val = 0
-    morning_num = 0
-    lunch_num = 0
-    dinner_num = 0
-    bed_num = 0
-
+    
+    total_value = 0
+    len_glucose = 0
     # get data from json
     for x in inputs:
         timestamp = x['timestamp']
@@ -42,32 +36,60 @@ def address():
         assert isinstance(value, float)
 
         time = datetime.strptime(timestamp, '%Y-%m-%d %H:%M')
-        hour = time.hour
-        if 4 <= hour <= 10:
-            morning_val += value
-            morning_num += 1
-        elif 11 <= hour <= 15:
-            lunch_val += value
-            lunch_num += 1
-        elif 16 <= hour <= 21:
-            dinner_val += value
-            dinner_num += 1
-        else:
-            bed_val += value
-            bed_num += 1
+        next_day = time + timedelta(days=1)
 
+        if code in range(57, 64):
+            total_value += value
+            glucose_val = value
+            len_glucose += 1
+
+    date = next_day.year, next_day.month, next_day.day
+    pre_breakfast_time = f'{date[0]}-{date[1]}-{date[2]} 07:23'
+    pre_breakfast_time = datetime.strptime(
+        pre_breakfast_time, '%Y-%m-%d %H:%M')
+    pre_breakfast_min = (pre_breakfast_time - time).total_seconds()/60
+    post_breakfast_time = f'{date[0]}-{date[1]}-{date[2]} 09:56'
+    post_breakfast_time = datetime.strptime(
+        post_breakfast_time, '%Y-%m-%d %H:%M')
+    post_breakfast_min = (post_breakfast_time -
+                          pre_breakfast_time).total_seconds()/60
+    pre_lunch_time = f'{date[0]}-{date[1]}-{date[2]} 12:09'
+    pre_lunch_time = datetime.strptime(pre_lunch_time, '%Y-%m-%d %H:%M')
+    pre_lunch_min = (pre_lunch_time - post_breakfast_time).total_seconds()/60
+    post_lunch_time = f'{date[0]}-{date[1]}-{date[2]} 14:20'
+    post_lunch_time = datetime.strptime(post_lunch_time, '%Y-%m-%d %H:%M')
+    post_lunch_min = (post_lunch_time - post_breakfast_time).total_seconds()/60
+    pre_supper_time = f'{date[0]}-{date[1]}-{date[2]} 17:52'
+    pre_supper_time = datetime.strptime(pre_supper_time, '%Y-%m-%d %H:%M')
+    pre_supper_min = (pre_supper_time - post_lunch_time).total_seconds()/60
+    post_supper_time = f'{date[0]}-{date[1]}-{date[2]} 19:11'
+    post_supper_time = datetime.strptime(post_supper_time, '%Y-%m-%d %H:%M')
+    post_supper_min = (post_supper_time - pre_supper_time).total_seconds()/60
     # unpickle
-    # with open('model.pickle', 'rb') as mod:
-    #     model = pickle.load(mod)
+    with open('model.pkl', 'rb') as mod:
+        # [predict code,
+        # given last glucose measurement,
+        # known avg measurement(all),
+        # minutes since last measurement]
+        model = pickle.load(mod)
+
+    avg_values = total_value/len_glucose
 
     # predict
-    # output = model.predict(timestamp, code, value)
+    pre_breakfast = model.predict([[58, glucose_val, avg_values, pre_breakfast_min]])
+    post_breakfast = model.predict([[59, pre_breakfast[0], avg_values, post_breakfast_min]])
+    pre_lunch = model.predict([[60, post_breakfast[0], avg_values, pre_lunch_min]])
+    post_lunch = model.predict([[61, pre_lunch[0], avg_values, post_lunch_min]])
+    pre_supper = model.predict([[62, post_lunch[0], avg_values, pre_supper_min]])
+    post_supper = model.predict([[63, pre_supper[0], avg_values, post_supper_min]])
 
     # use a dictionary to format output for json
-    out = {'Morning blood glucose level': int(morning_val//morning_num),
-           'Noon blood glucose level': int(lunch_val//lunch_num),
-           'Evening blood glucose level': int(dinner_val//dinner_num),
-           'Night blood glucose level': int(bed_val//bed_num)}
+    out = {'Pre-breakfast measurement': pre_breakfast[0],
+           'Post-breakfast measurement': post_breakfast[0],
+           'Pre-lunch measurement': pre_lunch[0],
+           'Post-lunch measurement': post_lunch[0],
+           'Pre-supper measurement': pre_supper[0],
+           'Post-supper measurement': post_supper[0]}
 
     # give output to sender.
     return app.response_class(
